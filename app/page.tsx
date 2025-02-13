e5f1c737ac838faf4fccf33db14a530d3dfc6c9d
Leo Edwards <theactualleoedwards@gmail.com>
2025-02-13 21:56:43 +0800

    new beginnings - intial commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-4xl font-bold">
+        trueGrand
+      </h1>
+      <p className="mb-4">
+        {`Hello there! I'm Leo Edwards, you may know me by my pseudynom trueGrand!
+        As a high school student with little knowledge in the realm of computer science, entering into the 
+        tech scene has been eyeopening and inspiring. I would like to bring this experience to students all over.`}
+      </p>
+      
+      <p className="mb-4">
+        {`🚧 please note that this site is still under construction...`}
+      </p>
+
+    </section>
+  )
+}
+
+//       <div className="my-8">
+//        <BlogPosts />
+//        </div>
